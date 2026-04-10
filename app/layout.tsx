ccda543beba0184d710d9c8e4df3ae5626426b29
joravkumar <joravkumar@gmail.com>
2026-04-10 23:39:58 +0530

    fix: suppress hydration warning caused by Grammarly extension

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${bebasNeue.variable} ${dmSans.variable}`}>
+      <body className={`${bebasNeue.variable} ${dmSans.variable}`} suppressHydrationWarning>
         {children}
       </body>
     </html>
